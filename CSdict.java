65e86a3ede938fcd2f76ebfda1ddde8b3bec05f2
bnguyen86 <bnguyen@outlook.com>
2016-01-11 23:05:37 -0800

    Added command recognition to shell

--- CSdict.java
+++ CSdict.java
@@ -15,32 +15,68 @@ public class CSdict
     static Boolean debugOn = false;
     public static void main(String [] args)
     {
-	byte cmdString[] = new byte[MAX_LEN];
-	
-	if (args.length == PERMITTED_ARGUMENT_COUNT) {
-	    debugOn = args[0].equals("-d");
-	    if (debugOn) {
-		System.out.println("Debugging output enabled");
-	    } else {
-		System.out.println("997 Invalid command line option - Only -d is allowed");
-		return;
-            } 
-	} else if (args.length > PERMITTED_ARGUMENT_COUNT) {
-	    System.out.println("996 Too many command line options - Only -d is allowed");
-	    return;
-	}
+		byte cmdString[] = new byte[MAX_LEN];
 		
-	try {
-	    for (int len = 1; len > 0;) {
-		System.out.print("csdict> ");
-		len = System.in.read(cmdString);
-		if (len <= 0) 
-		    break;
-		// Start processing the command here.
-		System.out.println("900 Invalid command.");
-	    }
-	} catch (IOException exception) {
-	    System.err.println("998 Input error while reading commands, terminating.");
-	}
+		if (args.length == PERMITTED_ARGUMENT_COUNT) {
+		    debugOn = args[0].equals("-d");
+		    if (debugOn) {
+			System.out.println("Debugging output enabled");
+		    } else {
+			System.out.println("997 Invalid command line option - Only -d is allowed");
+			return;
+	            } 
+		} else if (args.length > PERMITTED_ARGUMENT_COUNT) {
+		    System.out.println("996 Too many command line options - Only -d is allowed");
+		    return;
+		}
+			
+		try {
+		    for (int len = 1; len > 0;) {
+				System.out.print("csdict> ");
+				len = System.in.read(cmdString);
+				if (len <= 0) 
+				    break;
+				// Start processing the command here.
+				else {
+					// Convert the cmdString byte array to a string and make all lowercase 
+					String cmd = new String(cmdString);
+					cmd = cmsd.toLowerCase();
+
+					if(cmd.startsWith("open")){
+						//TODO: handle command: open SERVER PORT
+
+					} else if(cmd.startsWith("dict")){
+						//TODO: handle command
+						
+					} else if(cmd.startsWith("set")){
+						//TODO: handle command: set DICTIONARY
+
+					} else if(cmd.startsWith("currdict")){
+						//TODO: handle command
+
+					} else if(cmd.startsWith("define")){
+						//TODO: handle command: define WORD
+
+					} else if(cmd.startsWith("match")){
+						//TODO: handle command: match WORD
+
+					} else if(cmd.startsWith("prefixmatch")){
+						//TODO: handle command: prefixmatch WORD
+
+					} else if(cmd.startsWith("close")){
+						//TODO: handle command
+
+					} else if(cmd.startsWith("quit")){
+						//TODO: handle command
+
+					} else{
+						System.out.println("900 Invalid command.");
+					}
+					cmdString = new byte[MAX_LEN];
+				}			
+		    }
+		} catch (IOException exception) {
+		    System.err.println("998 Input error while reading commands, terminating.");
+		}
     }
 }
