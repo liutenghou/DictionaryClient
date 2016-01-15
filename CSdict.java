c5cfff4a488c43368a4fd75cd86052096d291484
Liu Teng Hou <liutenghou@Tengs-MacBook-Air.local>
2016-01-14 21:50:15 -0800

    got input working with both command and options, maybe put options in another array for next step

--- CSdict.java
+++ CSdict.java
@@ -1,5 +1,6 @@
 
 import java.lang.System;
+import java.util.Scanner;
 import java.io.IOException;
 
 //
@@ -7,15 +8,22 @@ import java.io.IOException;
 // line dictionary client. The program takes no arguments.
 //
 
-
 public class CSdict
 {
     static final int MAX_LEN = 255;
     static final int PERMITTED_ARGUMENT_COUNT = 1;
     static Boolean debugOn = false;
+    
+    //error messages
+    static String e900 = "900 Invalid command";
+    static String e901 = "901 Incorrect number of arguments";
+    static String e902 = "902 Invalid argument";
+    static String e903 = "903 Supplied command not expected at this time";
+    
     public static void main(String [] args)
     {
 		byte cmdString[] = new byte[MAX_LEN];
+		Scanner scan = new Scanner(System.in);
 		
 		if (args.length == PERMITTED_ARGUMENT_COUNT) {
 		    debugOn = args[0].equals("-d");
@@ -23,97 +31,102 @@ public class CSdict
 			System.out.println("Debugging output enabled");
 		    } else {
 			System.out.println("997 Invalid command line option - Only -d is allowed");
+			scan.close();
 			return;
 	            } 
 		} else if (args.length > PERMITTED_ARGUMENT_COUNT) {
 		    System.out.println("996 Too many command line options - Only -d is allowed");
+		    scan.close();
 		    return;
 		}
-			
-		try {
-		    for (int len = 1; len > 0;) {
-				System.out.print("csdict> ");
-				len = System.in.read(cmdString);
-				if (len <= 0) 
-				    break;
-				// Start processing the command here.
-				else {
-					// Convert the cmdString byte array to a string and make all lowercase
-					String inputString = new String(cmdString);
-					inputString = inputString.trim();
-
-					String cmd;
-					if(inputString.indexOf(" ") >  0){
-						cmd = inputString.substring(0 , inputString.indexOf(" ")).toLowerCase();
-					} else{
-						cmd = inputString.toLowerCase();
-					}
-					
-
-					if(cmd.startsWith("open")){
-						//TODO: handle command: open SERVER PORT
-						String[] params = extractParamerters("open" , cmd);
-						for(String param: params){
-							System.out.println("testing" + param);
-						}
-
-					} else if(cmd.startsWith("dict")){
-						//TODO: handle command
-						
-					} else if(cmd.startsWith("set")){
-						//TODO: handle command: set DICTIONARY
-						String[] params = extractParamerters("set" , cmd);
-
-					} else if(cmd.startsWith("currdict")){
-						//TODO: handle command
-
-					} else if(cmd.startsWith("define")){
-						//TODO: handle command: define WORD
-						String[] params = extractParamerters("define" , cmd);
-
-					} else if(cmd.startsWith("match")){
-						//TODO: handle command: match WORD
-						String[] params = extractParamerters("match" , cmd);
-
-					} else if(cmd.startsWith("prefixmatch")){
-						//TODO: handle command: prefixmatch WORD
-						String[] params = extractParamerters("prefixmatch" , cmd);
-
-					} else if(cmd.startsWith("close")){
-						//TODO: handle command
-
-					} else if(cmd.startsWith("quit")){
-						//TODO: handle command
-						break;
-					} else{
-						System.out.println("900 Invalid command.");
-					}
-					cmdString = new byte[MAX_LEN];
-				}			
-		    }
-		} catch (IOException exception) {
-		    System.err.println("998 Input error while reading commands, terminating.");
+		
+		//read input
+		try{
+			inputHandling(scan);
+		} catch (Exception e) {
+		    System.err.println("Something happened: "+ e.getMessage());
+		    inputHandling(scan);
 		}
+	    scan.close();	
+    }
+    
+    public static void inputHandling(Scanner scan){
+    	while(true) {
+			System.out.print("csdict> ");
+			String inputString = scan.nextLine();
+			inputString = inputString.trim();
+			
+			System.out.println("You typed: "+inputString); //TEST
+			
+			//input string to array, delimited by 0 or more spaces or tabs
+			String[] inputStringArray = inputString.split("[ *\t*]");
+			if(inputStringArray.length > 3){
+				System.out.println(e901);
+				continue;
+			}
+			
+			//TEST: input is read correctly
+			for(String x: inputStringArray){
+				System.out.println(x);
+			}
+			
+			//first input to cmd, converted to all lower case
+			String cmd = inputStringArray[0].toLowerCase();
+			
+			if (inputString.length() <= 0){ //length of input too short
+			    continue;
+			    
+			// Start processing the command here.
+			} else if(cmd.equals("#")){
+				continue;
+				
+    		} else if(cmd.equals("open")){
+				//TODO: handle command: open SERVER PORT
+				System.out.println("Inside Open: " + cmd);
+				String portNumber = "2628";
+				//if missing port, use default 2628
+				if(inputStringArray.length == 3){
+					portNumber = inputStringArray[2];
+				}else if(inputStringArray.length <= 1 || inputStringArray.length > 3){
+					System.out.println(e901);
+					continue;
+				}
+				
+
+			} else if(cmd.equals("dict")){
+				//TODO: handle command
+				
+			} else if(cmd.equals("set")){
+				//TODO: handle command: set DICTIONARY
+				
+
+			} else if(cmd.equals("currdict")){
+				//TODO: handle command
+
+			} else if(cmd.equals("define")){
+				//TODO: handle command: define WORD
+			
+
+			} else if(cmd.equals("match")){
+				//TODO: handle command: match WORD
+				
+
+			} else if(cmd.equals("prefixmatch")){
+				//TODO: handle command: prefixmatch WORD
+			
+
+			} else if(cmd.equals("close")){
+				//TODO: handle command
+
+			} else if(cmd.equals("quit")){
+				//TODO: handle command
+				break;
+			} else{
+				System.out.println("900 Invalid command.");
+			}
+		}	
+    	
     }
 
-    //command: the command that will be removed so we are only left with the params
-    //input: the whole input string
-    //will always return a String[] of size 2, with the second element being empty if there's
-    //only one param
-    private static String[] extractParamerters(String command, String input){
-    	String[] paramArr = new String[2];
-    	//remove the command from the input and trim the whitespace
-    	String paramStr = input.substring(command.length() , input.length()).trim();
-
-    	int firstSpaceIdx = paramStr.indexOf(" ");
-
-    	if(firstSpaceIdx < 0){
-    		paramArr[0] = paramStr;
-    	} else{
-    		paramArr[0] = paramStr.substring(0,firstSpaceIdx).trim();
-    		paramArr[1] = paramStr.substring(firstSpaceIdx + 1 , paramStr.length()).trim();
-    	}
-
-    	return paramArr;
-    }
+    
 }
